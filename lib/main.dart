7d1700214506d667464b7381256c424694b4b627
yui <y129may9th@gmail.com>
2024-01-18 22:00:01 +0900

    feat: initialized Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final messagingInstance = FirebaseMessaging.instance;
+  messagingInstance.requestPermission();
+
+  final fcmToken = await messagingInstance.getToken();
+  debugPrint('FCM TOKEN: $fcmToken');
+
   runApp(const MyApp());
 }
 
